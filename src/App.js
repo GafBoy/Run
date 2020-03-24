b457c2f81243228af4f74f2d23a2ce0d99abe972
Sportz <gafboy@MacBook-Pro-Sportz.local>
2020-03-24 16:18:00 +0300

    2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
-import {Route} from "react-router-dom";
+import { Route } from "react-router-dom";
 import HomePage from "./Components/pages/HomePage";
 import LoginPage from "./Components/pages/LoginPage";
 
-const App =() =><div className="ui container">
+const App = () => <div className="ui container">
   <Route path="/" exact component={HomePage} />
   <Route path="/Login" exact component={LoginPage} />
 </div>
--- src/Components/pages/HomePage.js
+++ src/Components/pages/HomePage.js
@@ -1,9 +1,9 @@
-import React  from "react";
-import {Link} from "react-router-dom";
+import React from "react";
+import { Link } from "react-router-dom";
 
-const HomePage =() =><div>
- <h1>HomePage</h1>
- <Link to="/Login">Login</Link>
+const HomePage = () => <div>
+    <h1>HomePage</h1>
+    <Link to="/Login">Login</Link>
 </div>
 
 export default HomePage;
